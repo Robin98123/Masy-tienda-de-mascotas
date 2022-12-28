d3b53c2e82db1bf661bb6607e4691f5b2c47f2f4
Robin98123 <robinson1998sena@gmail.com>
2022-12-28 14:16:35 -0500

    se sube componnete de producto

--- src/components/ProductOff.jsx
+++ src/components/ProductOff.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./productoff.css"
 import product_image_1 from "../img/ofertas/ofertas_1.png"
+import car_icon from "../icons/car_prices.svg"
 
 export default function ProductOff (){
     return(
@@ -8,15 +9,18 @@ export default function ProductOff (){
             <div className="product_cointainter-off">
                 -10%
             </div>
-            <div>
+            <div className="image_prodcut">
             <img src={product_image_1} />
             </div>
-            <div>
-                <h1>Ganador premium- cachorro x 4kg</h1>
-            
-                <h2>$78.000</h2>
-                <h3>$70.200</h3>
-                <button></button>
+            <div className="price_continer">
+                <h1>Ganador premium- cachorro x 4kg</h1>  
+                <div className="price_description">  
+                    <div className="price_cash">     
+                    <h2>$78,000</h2>
+                    <h3>$70,200</h3>
+                    </div> 
+                <button className="car_button"><img src={car_icon}/></button>
+                </div> 
             </div>
 
         </div>
