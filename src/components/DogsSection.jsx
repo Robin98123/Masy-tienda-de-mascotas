cec67966b7c5bd2a0788fb0e9b4ccde627591eda
Robin98123 <robinson1998sena@gmail.com>
2023-01-10 11:04:33 -0500

    se inicia categoria de productos

---
+++ src/components/DogsSection.jsx
@@ -0,0 +1,164 @@
+import React from "react";
+import "./dogssection.css";
+import dogs_image_1 from "../img/sections/dogs/dogs_1.png"
+import car_icon from "../icons/car_prices.svg"
+
+export default function DogSection (){
+    return(
+        <>
+        <div className="container_product">
+        <div className="product_container">
+            <div className="image_prodcut">
+            <img src={dogs_image_1} />
+            </div>
+            <div className="price_continer">
+                <h1>Ganador premium- cachorro x 4kg</h1>  
+                <div className="price_description">  
+                    <div className="price_cash">
+                    <h3>$70,200</h3>
+                    </div> 
+                <button className="car_button"><img src={car_icon}/></button>
+                </div> 
+            </div>
+
+        </div>
+
+        <div className="product_container">
+            <div className="image_prodcut">
+            <img src={dogs_image_1} />
+            </div>
+            <div className="price_continer">
+                <h1>Ganador premium- cachorro x 4kg</h1>  
+                <div className="price_description">  
+                    <div className="price_cash">
+                    <h3>$70,200</h3>
+                    </div> 
+                <button className="car_button"><img src={car_icon}/></button>
+                </div> 
+            </div>
+
+        </div>
+
+        <div className="product_container">
+            <div className="image_prodcut">
+            <img src={dogs_image_1} />
+            </div>
+            <div className="price_continer">
+                <h1>Ganador premium- cachorro x 4kg</h1>  
+                <div className="price_description">  
+                    <div className="price_cash">
+                    <h3>$70,200</h3>
+                    </div> 
+                <button className="car_button"><img src={car_icon}/></button>
+                </div> 
+            </div>
+
+        </div>
+        </div>
+
+<div className="container_product">
+<div className="product_container">
+    <div className="image_prodcut">
+    <img src={dogs_image_1} />
+    </div>
+    <div className="price_continer">
+        <h1>Ganador premium- cachorro x 4kg</h1>  
+        <div className="price_description">  
+            <div className="price_cash">
+            <h3>$70,200</h3>
+            </div> 
+        <button className="car_button"><img src={car_icon}/></button>
+        </div> 
+    </div>
+
+</div>
+
+<div className="product_container">
+    <div className="image_prodcut">
+    <img src={dogs_image_1} />
+    </div>
+    <div className="price_continer">
+        <h1>Ganador premium- cachorro x 4kg</h1>  
+        <div className="price_description">  
+            <div className="price_cash">
+            <h3>$70,200</h3>
+            </div> 
+        <button className="car_button"><img src={car_icon}/></button>
+        </div> 
+    </div>
+
+</div>
+
+<div className="product_container">
+    <div className="image_prodcut">
+    <img src={dogs_image_1} />
+    </div>
+    <div className="price_continer">
+        <h1>Ganador premium- cachorro x 4kg</h1>  
+        <div className="price_description">  
+            <div className="price_cash">
+            <h3>$70,200</h3>
+            </div> 
+        <button className="car_button"><img src={car_icon}/></button>
+        </div> 
+    </div>
+
+</div>
+</div>
+
+<div className="container_product">
+<div className="product_container">
+    <div className="image_prodcut">
+    <img src={dogs_image_1} />
+    </div>
+    <div className="price_continer">
+        <h1>Ganador premium- cachorro x 4kg</h1>  
+        <div className="price_description">  
+            <div className="price_cash">
+            <h3>$70,200</h3>
+            </div> 
+        <button className="car_button"><img src={car_icon}/></button>
+        </div> 
+    </div>
+
+</div>
+
+<div className="product_container">
+    <div className="image_prodcut">
+    <img src={dogs_image_1} />
+    </div>
+    <div className="price_continer">
+        <h1>Ganador premium- cachorro x 4kg</h1>  
+        <div className="price_description">  
+            <div className="price_cash">
+            <h3>$70,200</h3>
+            </div> 
+        <button className="car_button"><img src={car_icon}/></button>
+        </div> 
+    </div>
+
+</div>
+
+<div className="product_container">
+    <div className="image_prodcut">
+    <img src={dogs_image_1} />
+    </div>
+    <div className="price_continer">
+        <h1>Ganador premium- cachorro x 4kg</h1>  
+        <div className="price_description">  
+            <div className="price_cash">
+            <h3>$70,200</h3>
+            </div> 
+        <button className="car_button"><img src={car_icon}/></button>
+        </div> 
+    </div>
+
+</div>
+</div>
+
+
+
+</>
+
+    )
+}
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,12 +1,14 @@
 import "../components/shop.css"
 import ProductOff from "../components/ProductOff";
 import Sections from "../components/Sections";
+import Dogs from "../components/DogsSection"
 
 const Shop = () =>{
     return (
         <div className="Shop-container">
         <ProductOff/>
         <Sections/>
+        <Dogs/>
         </div>
     )
 } 
