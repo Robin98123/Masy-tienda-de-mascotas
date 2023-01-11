7f3bf2003e44e7a3511a5184c66170ad709633b9
Robin98123 <robinson1998sena@gmail.com>
2023-01-10 20:22:11 -0500

    se sube tarjeta formato array

---
+++ src/img/ProdutsCards/data.js
@@ -0,0 +1,10 @@
+import perros_img_1 from "../../img/sections/dogs/dogs_1.png"
+
+export const data = [
+    {
+      id: 1,
+      name: "Ganador premium- cachorro x 4kg",
+      price: 70.200,
+      imgs:  {perros_img_1},
+    }
+  ];
